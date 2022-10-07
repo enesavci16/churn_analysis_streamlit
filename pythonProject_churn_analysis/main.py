35cd99845601388ef8da8e0bf6628370ea3a7d03
enesavci16 <83986093+enesavci16@users.noreply.github.com>
2022-10-07 22:44:15 +0300

    Update main.py

--- pythonProject_churn_analysis/main.py
+++ pythonProject_churn_analysis/main.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pickle
 import numpy as np
 import pandas as pd
-import seaborn as sns
+
 import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
